4af0a7bbd6291af5881e63daa3cb4a008389db13
EliW09 <eli@dogdigger.com>
2018-10-31 16:11:19 -0400

    Setup design
    Added design and a basic check to see if JQuery works correctly.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,6 +6,16 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
+const events = require('./events')
+
 $(() => {
-  // your JS code goes here
+  $('#box1').on('click', events.box1Stuff)
+  $('#box2').on('click', events.box2Stuff)
+  $('#box3').on('click', events.box3Stuff)
+  $('#box4').on('click', events.box4Stuff)
+  $('#box5').on('click', events.box5Stuff)
+  $('#box6').on('click', events.box6Stuff)
+  $('#box7').on('click', events.box7Stuff)
+  $('#box8').on('click', events.box8Stuff)
+  $('#box9').on('click', events.box9Stuff)
 })
---
+++ assets/scripts/events.js
@@ -0,0 +1,47 @@
+const box1Stuff = (event) => {
+  $('#box1').text('X')
+}
+
+const box2Stuff = (event) => {
+  $('#box2').text('O')
+}
+
+const box3Stuff = (event) => {
+  $('#box3').text('X')
+}
+
+const box4Stuff = (event) => {
+  $('#box4').text('O')
+}
+
+const box5Stuff = (event) => {
+  $('#box5').text('X')
+}
+
+const box6Stuff = (event) => {
+  $('#box6').text('O')
+}
+
+const box7Stuff = (event) => {
+  $('#box7').text('X')
+}
+
+const box8Stuff = (event) => {
+  $('#box8').text('O')
+}
+
+const box9Stuff = (event) => {
+  $('#box9').text('X')
+}
+
+module.exports = {
+  box1Stuff,
+  box2Stuff,
+  box3Stuff,
+  box4Stuff,
+  box5Stuff,
+  box6Stuff,
+  box7Stuff,
+  box8Stuff,
+  box9Stuff
+}
